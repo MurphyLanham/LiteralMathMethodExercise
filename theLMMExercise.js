5fa43c22c4bcd7602e71fb30fb343af13a470707
Murphy Lanham <murphylanham@KCCCs-MacBook-Pro.local>
2021-02-25 08:54:21 -0500

    Initial Commit

---
+++ theLMMExercise.js
@@ -0,0 +1,13 @@
+let warmHugs = "Hi, I'm Olaf and I like warm hugs.";
+console.log(warmHugs.toUpperCase());
+console.log(warmHugs.replace("like","love"));
+let beenImpaled = "Oh, look at that. I've been impaled.";
+console.log(beenImpaled.slice(18, 36));
+let dotDOtDot = "...";
+let skullBones = `I don't have a skull${dotDOtDot}or bones` 
+console.log(Math.PI);
+let randomNumber = Math.random();
+randomNumber *= 3;
+randomNumber = Math.floor(randomNumber);
+randomNumber++;
+console.log(randomNumber);
